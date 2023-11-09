904aa60549776823566f8a13b3be5cb442aadd1c
BRIJESH <122820661+Brijesh0707@users.noreply.github.com>
2023-11-09 13:05:44 +0530

    fixing ui

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -5,7 +5,7 @@ function InputField({label, type, name, placeholder, require}) {
       <div className="mr-[24px] mt-[24px] w-full">
           <p className="text-custom-text-darkGrey text-[16px] mb-[10px]">
               {label}
-              {require && <span className="text-red-500">*</span>}
+               <span className="text-red-500">*</span>
           </p>
           <input name= {name} type={type} placeholder={placeholder} className="text-[18px] border border-grayLight py-[6px] px-[12px] rounded focus:outline-none w-full"/>
       </div>
